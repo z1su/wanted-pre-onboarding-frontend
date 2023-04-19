86959e5c20ac354c91eca4a3fdd5dd57588f2119
Song ji su <thdwltn0727@gmail.com>
2023-04-19 15:28:21 +0900

    [CHORE] 파일 경로 변경

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import TodoPage from "./page/TodoPage";
+import SignUpPage from "./page/SignUpPage";
+import SignInPage from "./page/SignInPage";
+import { GlobalStyle } from "./style/globalStyle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/todo" element={<TodoPage />} />
+        <Route path="/" element={<SignUpPage />} />
+      </Routes>
+    </>
   );
 }
 
