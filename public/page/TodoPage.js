4e520487df926c828036b499970244c81ed6c758
Song ji su <thdwltn0727@gmail.com>
2023-04-19 15:16:09 +0900

    Revert "[FEAT] Todo API"
    This reverts commit 553f98e70ff814224c697688324fc58bb3b9123c.

--- public/page/TodoPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import TodoProvider from "../provider/todo/TodoProvider";
-import useAuthRedirect from "../hooks/useAuthRedirect";
-
-export default function TodoPage(props) {
-  useAuthRedirect();
-  return <TodoProvider />;
-}
